81ddc77c24d43fccd82de541a2172bc4cfb80e78
Alysson de Assis <alyssondeassis@MacBook-Pro-de-Alysson.local>
2020-04-04 19:58:20 -0300

    debug

--- prototipo/settings.py
+++ prototipo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('u4_3m_1kiqy5s9atr2(!cuo9qb2jak9f3r6q5z$c9be^%aq_rn')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
