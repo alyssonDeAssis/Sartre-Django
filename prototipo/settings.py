c3aecb5e3ba73560ac7634727c87cb0b6f9c5a26
Alysson de Assis <alyssondeassis@MacBook-Pro-de-Alysson.local>
2020-04-04 19:20:42 -0300

    Configuring the app

--- prototipo/settings.py
+++ prototipo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u4_3m_1kiqy5s9atr2(!cuo9qb2jak9f3r6q5z$c9be^%aq_rn'
+SECRET_KEY = config('u4_3m_1kiqy5s9atr2(!cuo9qb2jak9f3r6q5z$c9be^%aq_rn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'app-sartre-spt.herokuapp.com'
+]
 
 
 # Application definition
@@ -77,15 +81,9 @@ WSGI_APPLICATION = 'prototipo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spt',
-        'USER': 'postgres',
-        'PASSWORD': 'Rodrigo',
-        'HOST': 'localhost',
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
